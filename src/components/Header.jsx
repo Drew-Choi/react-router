3a8bcffc19446defc73383be4d6d48889e19cdbc
Drew <drc920630@gmail.com>
2023-04-04 09:42:47 +0900

    네비게이트 적용 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <>
       <nav>
         <ul style={{ display: 'flex', justifyContent: 'space-around' }}>
           <li>
-            <Link to="/">홈으로 이동</Link>
+            <button onClick={() => navigate('/')}>홈으로 이동</button>
           </li>
           <li>
-            <Link to="/profile">프로필 페이지로 이동</Link>
+            <button onClick={() => navigate('/profile')}>
+              프로필 페이지로 이동
+            </button>
           </li>
           <li>
             <Link to="/board">게시판 페이지로 이동</Link>
